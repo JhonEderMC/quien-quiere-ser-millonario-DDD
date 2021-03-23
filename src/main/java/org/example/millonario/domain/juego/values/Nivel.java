074e7766bc77d0095964af334e50640e5c9f8475
jhon mc <kgbxblackx@gmail.com>
2021-03-23 18:00:05 -0500

    Se crea objeto de valor nivel del juego.

---
+++ src/main/java/org/example/millonario/domain/juego/values/Nivel.java
@@ -0,0 +1,31 @@
+package org.example.millonario.domain.juego.values;
+
+import co.com.sofka.domain.generic.ValueObject;
+
+import java.util.Objects;
+
+public class Nivel implements ValueObject<Integer> {
+    private final Integer nivel;
+
+    public Nivel(Integer nivel) {
+        if(nivel<0){
+            throw new  IllegalArgumentException("El nivel tiene que ser un numero mayor a 0");
+        }
+        if(nivel>5){
+            throw new  IllegalArgumentException("El nivel maximo es 5");
+        }
+        this.nivel = Objects.requireNonNull(nivel, "El nivel es requerido");
+    }
+    public Nivel(){
+        this.nivel=1; //por defecto
+    }
+
+    public Integer nivel() {
+        return nivel;
+    }
+
+    @Override
+    public Integer value() {
+        return this.nivel;
+    }
+}
