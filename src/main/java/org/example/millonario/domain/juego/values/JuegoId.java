6b273eb8f719a1d7df0f272cd0bdbeb1b109c3de
jhon mc <kgbxblackx@gmail.com>
2021-03-23 15:51:34 -0500

    Se crea identidad o objecto de valor juegoId para indentificar al juego

---
+++ src/main/java/org/example/millonario/domain/juego/values/JuegoId.java
@@ -0,0 +1,10 @@
+package org.example.millonario.domain.juego.values;
+
+import co.com.sofka.domain.generic.Identity;
+
+public class JuegoId  extends Identity {
+
+    public JuegoId(String uuid) {
+        super(uuid);
+    }
+}
