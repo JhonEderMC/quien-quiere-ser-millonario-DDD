fb9858c6016a101235df7109d4da9599c495b7b3
jhon mc <kgbxblackx@gmail.com>
2021-03-23 16:38:21 -0500

    Se crea la entidad jugador.

---
+++ src/main/java/org/example/millonario/domain/juego/Jugador.java
@@ -0,0 +1,42 @@
+package org.example.millonario.domain.juego;
+
+import co.com.sofka.domain.generic.Entity;
+import org.example.millonario.domain.juego.values.*;
+
+public class Jugador extends Entity<JugadorId> {
+
+    private final Nombre nombre;
+    private final Profesion profesion;
+    private final telefonoAyudaAmigo telefonoAmigo;
+    private Capital capital;
+
+    public Jugador(JugadorId entityId, Nombre nombre, Profesion profesion, telefonoAyudaAmigo telefonoAmigo, Capital capital) {
+        super(entityId);
+        this.nombre = nombre;
+        this.profesion = profesion;
+        this.telefonoAmigo = telefonoAmigo;
+        this.capital = capital;
+    }
+
+    public void aumentarCapital(Capital capital){
+        this.capital = this.capital.aumentar(capital.value());
+    }
+
+
+
+    public Nombre nombre() {
+        return nombre;
+    }
+
+    public Profesion profesion() {
+        return profesion;
+    }
+
+    public telefonoAyudaAmigo telefonoAmigo() {
+        return telefonoAmigo;
+    }
+
+    public Capital capital() {
+        return capital;
+    }
+}
