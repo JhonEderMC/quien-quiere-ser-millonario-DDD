0749ed1307e35ed592c28b9f0ffb7763813212d2
jhon mc <kgbxblackx@gmail.com>
2021-03-23 18:26:51 -0500

    Se crea objeto de valor que contiene la informacion del telefono para ayuda de un amigo

---
+++ src/main/java/org/example/millonario/domain/juego/values/TelefonoAyudaAmigo.java
@@ -0,0 +1,21 @@
+package org.example.millonario.domain.juego.values;
+
+import co.com.sofka.domain.generic.ValueObject;
+
+import java.util.Objects;
+
+public class TelefonoAyudaAmigo implements ValueObject<String>{
+    private final String value;
+
+    public TelefonoAyudaAmigo(String telefono) {
+        if(telefono.isBlank()){
+            throw  new IllegalArgumentException("El telefono no puede ser vacio");
+        }
+        this.value = Objects.requireNonNull(telefono, "El telefono no puede ser nulo");
+    }
+
+    @Override
+    public String value() {
+        return this.value;
+    }
+}
