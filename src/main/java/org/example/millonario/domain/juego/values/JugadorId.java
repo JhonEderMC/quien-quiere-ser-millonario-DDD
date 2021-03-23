38e6200b56886e4d6ec3513ca732c814b1aa6240
jhon mc <kgbxblackx@gmail.com>
2021-03-23 16:36:58 -0500

    Se crea el identificador del jugador

---
+++ src/main/java/org/example/millonario/domain/juego/values/JugadorId.java
@@ -0,0 +1,10 @@
+package org.example.millonario.domain.juego.values;
+
+import co.com.sofka.domain.generic.Identity;
+
+public class JugadorId extends Identity {
+
+    public JugadorId(String uuid) {
+        super(uuid);
+    }
+}
