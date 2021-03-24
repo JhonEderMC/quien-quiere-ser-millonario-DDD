65316ca410cd142347391e8a4d5e7a42969b023a
jhon mc <kgbxblackx@gmail.com>
2021-03-23 23:58:55 -0500

    Se crea el comando para crear un caso de uso a la hora de crear una pregunta.

---
+++ src/main/java/org/example/millonario/domain/juego/command/CrearPregunta.java
@@ -0,0 +1,34 @@
+package org.example.millonario.domain.juego.command;
+
+import co.com.sofka.domain.generic.Command;
+import org.example.millonario.domain.juego.Respuesta;
+import org.example.millonario.domain.juego.values.Descripcion;
+import org.example.millonario.domain.juego.values.PreguntaId;
+
+import java.util.Set;
+
+public class CrearPregunta implements Command {
+
+    private final PreguntaId preguntaId;
+    private final Descripcion descripcion;
+    private final Set<Respuesta> respuestas;
+
+
+    public CrearPregunta(PreguntaId preguntaId, Descripcion descripcion, Set<Respuesta> respuestas) {
+        this.preguntaId = preguntaId;
+        this.descripcion = descripcion;
+        this.respuestas = respuestas;
+    }
+
+    public PreguntaId preguntaId() {
+        return preguntaId;
+    }
+
+    public Descripcion descripcion() {
+        return descripcion;
+    }
+
+    public Set<Respuesta> respuestas() {
+        return respuestas;
+    }
+}
