d5f30ff7697033d9313292d7c8fbbeb0bf448d0d
jhon mc <kgbxblackx@gmail.com>
2021-03-23 23:57:24 -0500

    Se agregan los eventos de que hacer en el agregado cuando un evento de crear una pregunta ha pasado.

--- src/main/java/org/example/millonario/domain/juego/CambiosJuego.java
+++ src/main/java/org/example/millonario/domain/juego/CambiosJuego.java
@@ -3,6 +3,7 @@ package org.example.millonario.domain.juego;
 import co.com.sofka.domain.generic.EventChange;
 import org.example.millonario.domain.juego.events.JuegoBase;
 import org.example.millonario.domain.juego.events.JugadorCreado;
+import org.example.millonario.domain.juego.events.PreguntaCreada;
 
 import java.util.HashMap;
 
@@ -18,6 +19,12 @@ public class CambiosJuego extends EventChange {
             juego.jugador = new Jugador(
                     event.jugadorId(), event.nombre(), event.profesion(), event.telefonoAmigo(),
                     event.capital()
+            );
+        });
+        apply((PreguntaCreada event)->{
+            juego.preguntas.put(
+                    event.preguntaId(),
+                    new Pregunta(event.preguntaId(), event.descripcion(), event.respuestas())
             );
         });
     }
