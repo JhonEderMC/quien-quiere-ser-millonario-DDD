68c70b8b5c58c66c044d1ad070395c34e5789ca7
jhon mc <kgbxblackx@gmail.com>
2021-03-23 23:56:41 -0500

    Se crea el comportamiento para poder agregar una pregunta.

--- src/main/java/org/example/millonario/domain/juego/Juego.java
+++ src/main/java/org/example/millonario/domain/juego/Juego.java
@@ -4,10 +4,12 @@ import co.com.sofka.domain.generic.AggregateEvent;
 import co.com.sofka.domain.generic.DomainEvent;
 import org.example.millonario.domain.juego.events.JuegoBase;
 import org.example.millonario.domain.juego.events.JugadorCreado;
+import org.example.millonario.domain.juego.events.PreguntaCreada;
 import org.example.millonario.domain.juego.values.*;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 public class Juego extends AggregateEvent<JuegoId> {
 
@@ -37,5 +39,9 @@ public class Juego extends AggregateEvent<JuegoId> {
 
     public void crearJugador(JugadorId jugadorId, Nombre nombre, Profesion profesion, TelefonoAyudaAmigo telefonoAmigo,Capital capital){
         appendChange(new JugadorCreado(jugadorId, nombre, profesion, telefonoAmigo,capital)).apply();
+    }
+
+    public void agregarPregunta(PreguntaId preguntaId, Descripcion descripcion, Set<Respuesta> respuestas ){
+        appendChange(new PreguntaCreada(preguntaId,descripcion, respuestas)).apply();
     }
 }
