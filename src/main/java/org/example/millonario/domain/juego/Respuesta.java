3c24051e35738b0381484ecaa6b5ca8b8b34b766
jhon mc <kgbxblackx@gmail.com>
2021-03-23 23:51:24 -0500

    Se crea objeto de valor respuesta que implementa descripcion y estado.
    Este objeto se usara como las respuestas a las preguntas.

---
+++ src/main/java/org/example/millonario/domain/juego/Respuesta.java
@@ -0,0 +1,41 @@
+package org.example.millonario.domain.juego;
+
+import co.com.sofka.domain.generic.ValueObject;
+import org.example.millonario.domain.juego.values.Descripcion;
+
+import java.util.Objects;
+
+public class Respuesta implements ValueObject<Respuesta.Value> {
+
+    private final Descripcion descripcion;
+    private final Estado estado;
+
+    public Respuesta(Descripcion descripcion, Estado estado) {
+        this.descripcion = descripcion;
+        this.estado = estado;
+    }
+
+    public static Respuesta of (Descripcion descripcion, Estado estado){
+        return  new Respuesta(descripcion, estado);
+    }
+
+    @Override
+    public Value value() {
+        return new Value() {
+            @Override
+            public Descripcion descripcion() {
+                return descripcion;
+            }
+
+            @Override
+            public Estado estado() {
+                return estado;
+            }
+        };
+    }
+
+    public interface Value{
+        Descripcion descripcion();
+        Estado estado();
+    }
+}
