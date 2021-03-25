f1abc61994d46a0fb447824c65fc65fcfd2e05f2
jhon mc <kgbxblackx@gmail.com>
2021-03-24 22:56:21 -0500

    Se crea el caso de uso para retirar un jugador

---
+++ src/main/java/org/example/millonario/domain/usecase/juego/RetirarVoluntarioJugadorUseCase.java
@@ -0,0 +1,23 @@
+package org.example.millonario.domain.usecase.juego;
+
+import co.com.sofka.business.generic.UseCase;
+import co.com.sofka.business.support.RequestCommand;
+import co.com.sofka.business.support.ResponseEvents;
+import org.example.millonario.domain.juego.Juego;
+import org.example.millonario.domain.juego.command.RetirarVoluntarioJugador;
+import org.example.millonario.domain.juego.values.JuegoId;
+
+public class RetirarVoluntarioJugadorUseCase extends UseCase<RequestCommand<RetirarVoluntarioJugador>, ResponseEvents> {
+
+    @Override
+    public void executeUseCase(RequestCommand<RetirarVoluntarioJugador> retirarVoluntarioJugadorRequestCommand) {
+        var command = retirarVoluntarioJugadorRequestCommand.getCommand();
+        var juegoId = new JuegoId();
+
+        var juego = Juego.from(juegoId, retrieveEvents());
+        juego.retirarJugador(command.getJugadorId());
+
+        emit().onResponse(new ResponseEvents(juego.getUncommittedChanges()));
+
+    }
+}
