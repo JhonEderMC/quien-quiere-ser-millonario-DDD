796a3f8bac660c475c37d03be0fa641e64a21ca4
jhon mc <kgbxblackx@gmail.com>
2021-03-29 15:18:11 -0500

    Se añade handle juego inicializado. Se corrige problema  la hora de actualizar las respuestas (guardarlas), ahora se guardan en orden 1,2,3,4.

--- src/main/java/org/example/millonario/infra/handle/JuegoMaterialize.java
+++ src/main/java/org/example/millonario/infra/handle/JuegoMaterialize.java
@@ -2,7 +2,7 @@ package org.example.millonario.infra.handle;
 
 
 import co.com.sofka.domain.generic.DomainEvent;
-import org.example.millonario.domain.juego.Pregunta;
+import org.example.millonario.domain.juego.JuegoIniciado;
 import org.example.millonario.domain.juego.events.*;
 import org.example.millonario.domain.juego.values.Respuesta;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -55,6 +55,16 @@ public class JuegoMaterialize {
 
     @Async
     @EventListener
+    public void handledEventIniciarJuegoIniciado(JuegoIniciado juegoIniciado){
+        logger.info("****** Handle event jugadorCreado");
+        Update update = new Update();
+        update.set("juegoInicializado", true);
+
+        mongoTemplate.updateFirst(getFilterByAggregateId(juegoIniciado), update, COLLECTION_NAME);
+    }
+
+    @Async
+    @EventListener
     public void handledEventPreguntaCreada(PreguntaCreada preguntaCreada){
         logger.info("****** Handle event preguntaCreada");
         Update update = new Update();
@@ -62,9 +72,11 @@ public class JuegoMaterialize {
         update.set("preguntaId",id);
         update.set("pregunta."+id+".descripcion",preguntaCreada.descripcion().value());
         Set<Respuesta> respuestas = preguntaCreada.respuestas();
+        int i=0;
         for (Respuesta respuesta: respuestas){
-            update.set("respuesta."+id+".descripcion",respuesta.value().descripcion().value());
-            update.set("respuesta."+id+".estado",respuesta.value().estado().value());
+            i++;
+            update.set("respuesta."+i+".descripcion",respuesta.value().descripcion().value());
+            update.set("respuesta."+i+".estado",respuesta.value().estado().value());
         }
 
         mongoTemplate.updateFirst(getFilterByAggregateId(preguntaCreada), update, COLLECTION_NAME);
@@ -76,7 +88,7 @@ public class JuegoMaterialize {
         logger.info("****** Handle event respuestajugador");
         Update update = new Update();
 
-        update.set("repuesta",respuestaJugador.respuestaJugador().value());
+        update.set("repuestaJugador",respuestaJugador.respuestaJugador().value());
 
         mongoTemplate.updateFirst(getFilterByAggregateId(respuestaJugador), update, COLLECTION_NAME);
     }
